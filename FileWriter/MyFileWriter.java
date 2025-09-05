9ce7c805f5334ca80330651ef876eeb43b2b84a8
HWRyan-Kim10 <rkim6@hwemail.com>
2025-09-04 23:41:22 -0700

    hidden file methods were added

--- FileWriter/MyFileWriter.java
+++ FileWriter/MyFileWriter.java
@@ -4,13 +4,14 @@ import java.nio.file.*;
 import java.nio.charset.StandardCharsets;
 
 public class MyFileWriter {
-    public statalsdkfjeic void main(String[] args) {
+    public static void main(String[] args) {
         String data = "Hello, World!";
         String fileName1 = "example.txt";
         String fileName2 = "example2.txt";
         String fileName3 = "example3.txt";
         String fileName4 = "example4.txt";
         String fileName5 = "example5.txt";
+
 
         // 1. Using FileWriter
         try (FileWriter writer = new FileWriter(fileName1)) {
@@ -45,6 +46,34 @@ public class MyFileWriter {
             Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        createHiddenPasswordFile("secrEtPassw0rd");
+        createConfidentialFile("11/30/25");
+    }
+
+    public static void createHiddenPasswordFile(String password) {
+        String fileName = ".verySecretFile.txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(password);
+            System.out.println("Hidden password file created successfully: " + fileName);
+        } 
+        catch (IOException e) {
+            System.out.println("Error writing to hidden password file: " + e.getMessage());
+        }
+    }
+
+    public static void createConfidentialFile(String info) {
+        String folderName = ".confidentialFolder";
+        String fileName = folderName + "/doNotShare.dat";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(info);
+            System.out.println("Hidden file created successfully in confidential folder: " + fileName);
+        } 
+        catch (IOException e) {
+            System.out.println("Error writing confidential file: " + e.getMessage());
         }
     }
 }
